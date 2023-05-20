c54948ca685ea0ba842e2fd1a3e30e4f47ad4b71
Thonyob <anthonyolivaresb@gmail.com>
2023-05-19 20:18:58 -0500

    primer commit2

--- src/main/java/com/banco/cuenta/bancocuenta/BancoCuentaApplication.java
+++ src/main/java/com/banco/cuenta/bancocuenta/BancoCuentaApplication.java
@@ -2,7 +2,7 @@ package com.banco.cuenta.bancocuenta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-    
+
 @SpringBootApplication
 public class BancoCuentaApplication {
 
